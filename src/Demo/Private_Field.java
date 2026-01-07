b7848e011946970d05a2c31fb01b209b05cf5c71
Anushka Jadhav <anujadhav32004@gmail.com>
2026-01-07 23:52:54 +0530

    First Commit

---
+++ src/Demo/Private_Field.java
@@ -0,0 +1,42 @@
+package Demo;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+public class Private_Field 
+{
+
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException 
+	{
+		
+			 Class<?> cl=City.class;
+			 Constructor<?> cs=cl.getDeclaredConstructor();
+			 
+			 Object obj=cs.newInstance();
+			 
+			 Field[] fields=cl.getDeclaredFields();
+			 
+			 
+			 for(Field f:fields)
+			 {
+				 System.out.println(f);
+			 }
+			 
+			 Field f=cl.getDeclaredField("name");
+			 f.setAccessible(true);
+			 f.set(obj, "helloPune");
+			 
+			 
+			 System.out.println(f.get(obj));
+			 
+	}
+}
+
+class City
+{
+	private String name;
+		
+	String pincode;
+}
+
